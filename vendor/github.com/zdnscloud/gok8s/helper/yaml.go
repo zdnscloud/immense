c3f7c15ef85d098a89684e7ae1f5b65ca7141e30
thesinglecloud <hanfeng@zdns.cn>
2019-05-30 05:22:52 +0000

    update gok8s

---
+++ vendor/github.com/zdnscloud/gok8s/helper/yaml.go
@@ -0,0 +1,41 @@
+package helper
+
+import (
+	"context"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/zdnscloud/gok8s/client"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+const YamlDelimiter = "---\n"
+
+func CreateResourceFromYaml(cli client.Client, yaml string) error {
+	return mapOnYamlDocument(yaml, cli.Create)
+}
+
+func DeleteResourceFromYaml(cli client.Client, yaml string) error {
+	return mapOnYamlDocument(yaml, func(ctx context.Context, obj runtime.Object) error {
+		return cli.Delete(ctx, obj)
+	})
+}
+
+func UpdateResourceFromYaml(cli client.Client, yaml string) error {
+	return mapOnYamlDocument(yaml, cli.Update)
+}
+
+func mapOnYamlDocument(yaml string, fn func(context.Context, runtime.Object) error) error {
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	for _, doc := range strings.Split(yaml, YamlDelimiter) {
+		obj, _, err := decode([]byte(doc), nil, nil)
+		if err != nil {
+			return err
+		}
+		if err := fn(context.TODO(), obj); err != nil {
+			return err
+		}
+	}
+	return nil
+}
